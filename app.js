d4d067ffec5425744fa5d7bef4fbd61e9d9e00e0
Malachy Okwute <mokwute@ivytech.edu>
2026-07-20 21:37:03 -0400

    added db - mo

--- app.js
+++ app.js
@@ -1,48 +1,53 @@
 // setup.. this is similar to when we use our default tags in html
 const express = require('express');
 
+const bodyParser = require('body-parser');
+const Song = require('./models/songs');
+
 // use cors in other to host frontend and backend on the same device. 
 // This is because the browser will block requests from different origins.
 let cors = require('cors');
 
 // store the express function in a variable called app
 const app = express();
-app.use(cors());
 
 // create a router object to handle routes
 const router = express.Router();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// all request that usually use an api starts with /api... e.g. the url would be http://localhost:3000/api/songs.
+app.use('/api', router);
 
 // start the server and listen on port 3000
 app.listen(3000, function () {
   console.log('Server is running on port 3000');
 });
 
-// making an api using routes.
-// routes are used to handle browser requests and send responses back to the browser.
-// This looks like URLs, the different is that when a browser requests a route, it is dynamically handled by using a functtion.
-
-router.get('/songs', function (req, res) {
-
-  const song = [
-    {
-      title: 'We found love',
-      artist: 'Rihanna',
-      popularity: 10,
-      genre: ['Pop', 'Dance'],
-      releaseDate: '2011-09-22'
-    },
-
-    {
-      title: 'Shape of you',
-      artist: 'Ed Sheeran',
-      popularity: 9,
-      genre: ['Pop', 'Dancehall'],
-      releaseDate: '2017-01-06'
-    }
-  ]
-  
-  res.json(song);
+// get all songs in db
+router.get('/songs', async (req, res) => {
+  try {
+    const songs = await Song.find({});
+    res.send(songs);
+    console.log(songs);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-// all request that usually use an api starts with /api... e.g. the url would be http://localhost:3000/api/songs.
-app.use('/api', router);
+// create a song
+router.post('/songs', async (req, res) => {
+  try {
+    const song = new Song(req.body);
+
+    await song.save();
+    res.status(201).json(song);
+    console.log(song);
+  } catch (error) {
+    res.status(400).json(error);
+    console.error(error);
+  }
+});
+
+// 
---
+++ db.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://sdev255:Password255@songdb.0g4s0g1.mongodb.net/?appName=SongDB");
+
+module.exports = mongoose;
